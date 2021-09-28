e90e7e54d404582cebb91228e771926007ed4436
Kiyomi Rodgers Denniston <kiyomi.rodgersdenniston@gmail.com>
2021-09-28 14:42:27 -0400

    finished

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let colors = document.querySelectorAll(".color");
+let selectedColor = document.querySelector("#current-color");
+
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    selectedColor.setAttribute("style", `background: ${color.style.background}`);
+  });
+});
+
+
+const cells = document.querySelectorAll(".cell");
+
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    cell.setAttribute("style", `background: ${selectedColor.style.background}`);
+  });
+});
